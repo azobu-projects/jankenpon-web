61d86058b770fa6f87fcd04b2c8375239f280bb9
M Haidar Hanif <git@mhaidarhanif.com>
2020-04-23 03:48:02 +0800

    Create home page

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Game from '../components/Game'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Game></Game>
+    </div>
+  )
+}
+
+export default HomePage
