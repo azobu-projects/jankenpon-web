4e385ceca0765d269615358b93196094c5092bb0
M Haidar Hanif <git@mhaidarhanif.com>
2020-04-23 04:56:48 +0800

    Use emoji images and rearrange game content layout

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,12 +8,17 @@ const GameContainer = styled.div`
   margin: 0 auto;
 `
 
+const GameContent = styled.div`
+  margin-top: 3rem;
+`
+
 const Heading = styled.h1`
-  text-align: center;
   font-family: 'Architects Daughter', cursive;
+  text-align: center;
 `
 
 const Section = styled.section`
+  font-family: 'Architects Daughter', cursive;
   padding: 1rem;
   margin: 1rem;
   border-radius: 5px;
@@ -24,9 +29,10 @@ const Section = styled.section`
 `
 
 const Player = styled(Section)`
+  flex-direction: row;
   border: 2px solid #000;
   font-size: 1.5em;
-  min-height: 100px;
+  height: 100px;
   background: ${(props) =>
     props.condition === 'win'
       ? '#7DE8A1' // gren
@@ -35,6 +41,16 @@ const Player = styled(Section)`
       : props.condition === 'draw'
       ? '#C7C7D1' // gray
       : '#FFFFFF'};
+`
+
+const PlayerName = styled.h3`
+  font-weight: bold;
+  margin: 0;
+`
+
+const PlayerChoice = styled.img`
+  height: 60px;
+  margin: 1rem;
 `
 
 const Result = styled(Section)`
@@ -52,6 +68,7 @@ const Panels = styled(Section)`
 
 const Button = styled.button`
   cursor: pointer;
+  font-weight: 900;
   padding: 1rem;
   background: #000;
   color: #fff;
@@ -63,16 +80,6 @@ const Button = styled.button`
     opacity: 0.8;
   }
   margin: 0 0.5rem;
-`
-
-const PlayerName = styled.h3`
-  font-weight: bold;
-  margin-top: 0;
-`
-
-const PlayerChoice = styled.p`
-  margin: 0;
-  text-transform: uppercase;
 `
 
 const Game = () => {
@@ -107,49 +114,47 @@ const Game = () => {
     <GameContainer>
       <Heading>Jankenpon Simple</Heading>
 
-      {/* Player Two or Computer */}
-      <Player condition={playerTwo.condition}>
-        <PlayerName>{playerTwo.name}:</PlayerName>
-        <PlayerChoice>
-          {getChoiceImage(playerTwo.choice)}
-          {playerTwo.choice}
-        </PlayerChoice>
-      </Player>
+      <GameContent>
+        {/* Player Two or Computer */}
+        <Player condition={playerTwo.condition}>
+          <PlayerName>{playerTwo.name}:</PlayerName>
+          {playerTwo.choice && (
+            <PlayerChoice src={`/images/${playerTwo.choice}.png`} />
+          )}
+        </Player>
 
-      {/* Game Result Message */}
-      <Result>
-        <span>{result.message}</span>
-      </Result>
+        {/* Game Result Message */}
+        <Result>
+          <span>{result.message}</span>
+        </Result>
 
-      {/* Player One */}
-      <Player condition={playerOne.condition}>
-        <PlayerName>{playerOne.name}:</PlayerName>
-        <PlayerChoice>
-          {getChoiceImage(playerOne.choice)}
-          {playerOne.choice}
-        </PlayerChoice>
-      </Player>
+        {/* Player One */}
+        <Player condition={playerOne.condition}>
+          <PlayerName>{playerOne.name}:</PlayerName>
+          <PlayerChoice src={`/images/${playerOne.choice}.png`} />
+        </Player>
 
-      {/* Game Panels */}
-      <Panels>
-        <Button
-          onClick={() => {
-            lockPlayerOne()
-            lockPlayerTwo()
-          }}
-        >
-          Set All Moves
-        </Button>
+        {/* Game Panels */}
+        <Panels>
+          <Button
+            onClick={() => {
+              lockPlayerOne()
+              lockPlayerTwo()
+            }}
+          >
+            Set All Moves
+          </Button>
 
-        <Button
-          onClick={() => {
-            const result = determineResult(playerOne, playerTwo)
-            setResult(result)
-          }}
-        >
-          Game On!
-        </Button>
-      </Panels>
+          <Button
+            onClick={() => {
+              const result = determineResult(playerOne, playerTwo)
+              setResult(result)
+            }}
+          >
+            Game On!
+          </Button>
+        </Panels>
+      </GameContent>
     </GameContainer>
   )
 }
