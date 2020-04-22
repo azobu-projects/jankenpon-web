41d9635d33ce4976ad029b010a1bc0be7a633835
M Haidar Hanif <git@mhaidarhanif.com>
2020-04-23 04:17:07 +0800

    Use game logic into component with layout

---
+++ src/components/Game.js
@@ -0,0 +1,134 @@
+import React, { useState } from 'react'
+import styled from '@emotion/styled'
+
+import { getRandomChoice, getChoiceImage, determineResult } from '../utils/game'
+
+const Section = styled.section`
+  padding: 1rem;
+  margin: 1rem;
+  border: 2px solid #000;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const Player = styled(Section)`
+  font-size: 1.5em;
+  min-height: 100px;
+`
+
+const Result = styled(Section)`
+  font-size: 1.5em;
+  text-transform: uppercase;
+  min-height: 30px;
+`
+
+const Panels = styled(Section)`
+  flex-direction: row;
+`
+
+const Button = styled.button`
+  cursor: pointer;
+  padding: 1rem;
+  background: #000;
+  color: #fff;
+  border: none;
+  font-size: 1em;
+  border-radius: 5px;
+  text-transform: uppercase;
+  &:hover {
+    opacity: 0.8;
+  }
+  margin: 0 0.5rem;
+`
+
+const PlayerName = styled.h3`
+  font-weight: bold;
+  margin-top: 0;
+`
+
+const PlayerChoice = styled.p`
+  margin: 0;
+  text-transform: uppercase;
+`
+
+const Game = () => {
+  const [playerOne, setPlayerOne] = useState({
+    name: 'You',
+    choice: 'rock',
+  })
+  const [playerTwo, setPlayerTwo] = useState({
+    name: 'Computer',
+    choice: '',
+  })
+  const [result, setResult] = useState({
+    message: ``,
+  })
+
+  const lockPlayerOne = () => {
+    setPlayerOne({
+      ...playerOne,
+    })
+  }
+
+  const lockPlayerTwo = () => {
+    setPlayerTwo({
+      ...playerTwo, // Use rest parameter to get existing keys
+      choice: getRandomChoice(),
+    })
+  }
+
+  return (
+    <div>
+      <h1>Jankenpon</h1>
+
+      {/* Player Two or Computer */}
+      <Player>
+        <PlayerName>{playerTwo.name}:</PlayerName>
+        <PlayerChoice>
+          {getChoiceImage(playerTwo.choice)}
+          {playerTwo.choice}
+        </PlayerChoice>
+      </Player>
+
+      {/* Game Result Message */}
+      <Result>
+        <span>{result.message}</span>
+      </Result>
+
+      {/* Player One */}
+      <Player>
+        <PlayerName>{playerOne.name}:</PlayerName>
+        <PlayerChoice>
+          {getChoiceImage(playerOne.choice)}
+          {playerOne.choice}
+        </PlayerChoice>
+      </Player>
+
+      {/* Game Panels */}
+      <Panels>
+        <Button
+          onClick={() => {
+            lockPlayerOne()
+            lockPlayerTwo()
+          }}
+        >
+          Set All Moves!
+        </Button>
+
+        <Button
+          onClick={() => {
+            const result = determineResult(playerOne, playerTwo)
+            setResult(result)
+          }}
+        >
+          Game On!
+        </Button>
+      </Panels>
+    </div>
+  )
+}
+
+export default Game
